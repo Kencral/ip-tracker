7edadc742446489e130d18601ceab4e4edc8b749
Kevin Cabral <kevincabral1@hotmail.com>
2022-12-28 20:09:20 -0400

    mobile version complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,69 @@
+import {useState, useLayoutEffect, useRef} from "react";
 import './App.css'
+import LocationMap from './LocationMap.jsx';
+
+const apiKey = 'at_p0Bs3hlM5wVoBC5hIgO18K1rxQbTb'; 
+const url = 'https://geo.ipify.org/api/v2/country,city?'; 
 
 function App() {
+  const [searchIp, setSearchIp] = useState("");
+
+  useLayoutEffect(getInfoIp, []);
+
+  function getInfoIp(domain = '', isIP = false){ 
+    const requestUrl= isIP ? `${url}apiKey=${apiKey}&ipAddress=${domain}` : domain ? `${url}apiKey=${apiKey}&domain=${domain}` : `${url}apiKey=${apiKey}`;
+    fetch(requestUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const formattedData = {
+          ip: data.ip,
+          isp: data.isp,
+          timezone: `UTC ${data.location.timezone}`,
+          location: `${data.location.country} ${data.location.region}`,
+          lat: data.location.lat,
+          lng: data.location.lng,
+          loading: false,
+        }
+        //infoIp.current = formattedData;
+        setSearchIp(formattedData);
+      })
+  }
+  function onSubmit (e) {
+    console.log(e.target[0].value);
+    e.preventDefault();
+    setSearchIp(previousState => {
+      return{...previousState, loading:true};
+    });
+    getInfoIp(e.target[0].value);
+  }
 
   return (
-    <h1>Hello World!</h1>
+    <>
+      <main>
+        <section className='bg-image'>
+        </section>
+        <section className='main-content'>
+          <h1>IP Address Tracker</h1>
+          <form onSubmit={onSubmit}>
+            <input type='text' placeholder='Search for any IP address or domain' />
+            <button type='submit'>
+              <span></span>
+            </button>
+          </form>
+          <dl>
+            <dt>Ip Address</dt>
+            <dd>{searchIp.ip}</dd>
+            <dt>Location</dt>
+            <dd>{searchIp.location}</dd>
+            <dt>Timezone</dt>
+            <dd>{searchIp.timezone}</dd>
+            <dt>ISP</dt>
+            <dd>{searchIp.isp}</dd>
+          </dl>
+        </section>
+      </main>
+      {(searchIp.lat && !searchIp.loading ) ? <LocationMap lat={searchIp.lat} lng={searchIp.lng} /> : <div>loading</div>}
+    </>
   )
 }
 
---
+++ src/LocationMap.jsx
@@ -0,0 +1,24 @@
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import L from 'leaflet';
+import iconURL from "./assets/icon-location.svg";
+
+const LocationMap = ({lat, lng}) => {
+  const position =  [lat, lng];
+  const myIcon = L.icon({
+    iconUrl: iconURL,
+    iconSize: [54, 60],
+    iconAnchor: [27,59],
+  })
+  return(
+    <MapContainer center={position} zoom={50} scrollWheelZoom={false} zoomControl={false}>
+      <TileLayer 
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={myIcon} > 
+      </Marker>
+    </MapContainer>
+  )
+}
+
+export default LocationMap;
