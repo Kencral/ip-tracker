357697348dad1e4087a9464461d8bef863314ca0
Kevin Cabral <kevincabral1@hotmail.com>
2023-01-04 02:16:54 -0400

    Configuration before deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ip-tracker/",
+  build:{
+    outDir: "Docs"
+  }
   plugins: [react()]
 })
